f3fa13a12dca1c2c616522154e7826e1392ce5c8
Brennan <68587169+brennan64@users.noreply.github.com>
2022-04-12 10:45:47 -0500

    Update follow.js
    slight change

--- commands/follow.js
+++ commands/follow.js
@@ -3,6 +3,6 @@ module.exports = {
     description: "this is a ping command",
     execute(message, args){
 
-        message.channel.send("Follow these guys, https://www.twitch.tv/dlens2\n https://www.twitch.tv/bsad\n https://www.twitch.tv/bre_guy");
+        message.channel.send("Follow these guys, https://www.twitch.tv/dlens2\n  https://www.twitch.tv/bre_guy");
     }
 }
